c62cd5f289251e6a937e6d715c26bde8694ef5b2
sbelkin <sabelkin@gmail.com>
2016-02-28 22:02:02 -0500

    Initializing a start to the reverse-words interviewcake.com project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # stunning-happiness
 Starting out with scala. 
+
+Found a few basic tutorials however, none seemed a good way to start. 
+
+So I am using a question from the website interviewcake.com 
+https://www.interviewcake.com/question/python/reverse-words
+
---
+++ src/scala/starting/Handler.scala
@@ -0,0 +1,23 @@
+package scala.starting
+
+/**
+  * Created by sbelkin on 2/28/2016.
+  */
+object Handler {
+
+  def main(args: Array[String]) {
+    if (args.length < 1) {
+      throw new IllegalArgumentException("The input variables must be at least 1 to read in file.")
+    }
+    print(args(0))
+    val source = scala.io.Source.fromFile(args(0))
+    val lines = try source.mkString finally source.close()
+    println(lines)
+  }
+
+
+  def reverse_words(args: String) : String = {
+    
+    ""
+  }
+}
