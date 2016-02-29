ee35f6ceaf76a72eee882626d33181c01ccefc32
sbelkin <sabelkin@gmail.com>
2016-02-29 00:53:53 -0500

    This doesnt solve the exact problem, its just a start to using scala

--- src/scala/starting/Handler.scala
+++ src/scala/starting/Handler.scala
@@ -9,15 +9,24 @@ object Handler {
     if (args.length < 1) {
       throw new IllegalArgumentException("The input variables must be at least 1 to read in file.")
     }
-    print(args(0))
     val source = scala.io.Source.fromFile(args(0))
     val lines = try source.mkString finally source.close()
-    println(lines)
+    var output = reverse_words(lines)
+
+    if(args.length < 2){
+      val source = scala.io.Source.fromFile(args(1))
+      val testlines = try source.mkString finally source.close()
+      assert(output == testlines)
+    } else {
+      println("No test input in second args.")
+      println(output)
+    }
   }
 
-
   def reverse_words(args: String) : String = {
-
-    ""
+    var split = args.split(" ").reverse
+    var reversed = ""
+    split.foreach( i => reversed += i + " " )
+    reversed
   }
 }
