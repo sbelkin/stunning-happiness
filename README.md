bb09d3ee437e4cd6842ba547ddb9e3f9309a1326
sbelkin <sabelkin@gmail.com>
2016-03-06 17:16:46 -0500

    Update, now just need to decided how neightbours are to be counted

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Starting out with scala.
 Found a few basic tutorials however, none seemed a good way to start. 
 
 Decided to make Conway's Game of Life.
+
+From the wiki we got the following rules:
+  Game of Life universe is a infinity two dimentional grid.
+  In each step the following transitions occur:
+    Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+    Any live cell with two or three live neighbours lives on to the next generation.
+    Any live cell with more than three live neighbours dies, as if by over-population.
+    Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
--- src/scala/starting/Handler.scala
+++ src/scala/starting/Handler.scala
@@ -13,26 +13,17 @@ object Handler {
 
   // Array of words, then reverse. then fix the words
   def main(args: Array[String]) {
-    if (args.length < 2) {
-      throw new IllegalArgumentException("The input variables must be at least 1 to read in file.")
+    if (args.length < 3) {
+      throw new IllegalArgumentException("The input variables must be row, colums and default file.")
     }
-//    starting_life(source)
-//    var grid: Array[Array[Integer]] = new Array[Array[Integer]]();
     rows = args(0).toInt
     cols = args(1).toInt
     var grid = empty_grid()
     grid = starting_life(args(2),grid)
+    println("Staring grid:")
     printing_grid(grid)
   }
 
-  def empty_grid() : Array[Array[Integer]] = {
-    val grid = Array.ofDim[Integer](rows,cols)
-    for {
-      i <- 0 until rows
-      j <- 0 until cols
-    } {grid(i)(j)=0}
-    grid
-  }
 
   def printing_grid(grid: Array[Array[Integer]]) : Unit = {
     grid.foreach(f => {
@@ -48,10 +39,49 @@ object Handler {
     } println(s"($i)($j) =${grid(i)(j)}")
   }
 
+  def empty_grid() : Array[Array[Integer]] = {
+    val grid = Array.ofDim[Integer](rows,cols)
+    for {
+      i <- 0 until rows
+      j <- 0 until cols
+    } {grid(i)(j)=0}
+    grid
+  }
+
   def starting_life(file: String, grid: Array[Array[Integer]]) : Array[Array[Integer]] = {
     val source = scala.io.Source.fromFile(file)
     val lines = try source.getLines.toArray finally source.close()
     lines.foreach(f=> { var temp = f.split(":"); grid(temp(0).toInt)(temp(1).toInt) = 1; })
     grid
+  }
+
+  def life_iteration(grid: Array[Array[Integer]]) : Array[Array[Integer]] = {
+    var new_grid = empty_grid()
+
+    grid
+  }
+
+  def count_neighbours(r:Integer, c:Integer): Integer = {
+    var neighbours: Integer = 0
+
+    neighbours
+  }
+
+  def live_cell_out(neighbours: Integer) : Integer = {
+    //    Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+    //    Any live cell with two or three live neighbours lives on to the next generation.
+    //    Any live cell with more than three live neighbours dies, as if by over-population.
+    if (2 == neighbours || 3 == neighbours) {
+      return 1
+    }
+    0
+  }
+
+  def dead_cell_out(neighbours: Integer) : Integer = {
+    //    Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+    if (3 == neighbours) {
+      return 1
+    }
+    0
   }
 }
