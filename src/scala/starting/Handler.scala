0b09dab3673a4c14f6661fda77377ca88931febe
sbelkin <sabelkin@gmail.com>
2016-03-06 18:45:47 -0500

    Game of life working need to take args to start it: Rows,Columns,DefaultFile,IterationLifeCount

--- src/scala/starting/Handler.scala
+++ src/scala/starting/Handler.scala
@@ -10,6 +10,7 @@ object Handler {
 
   var rows = 0
   var cols = 0
+  var grid = Array.ofDim[Integer](rows,cols)
 
   // Array of words, then reverse. then fix the words
   def main(args: Array[String]) {
@@ -18,25 +19,18 @@ object Handler {
     }
     rows = args(0).toInt
     cols = args(1).toInt
-    var grid = empty_grid()
-    grid = starting_life(args(2),grid)
+    grid = empty_grid()
+    grid = starting_life(args(2))
     println("Staring grid:")
     printing_grid(grid)
+    1 to args(3).toInt foreach { _ => life_iteration()}
   }
-
 
   def printing_grid(grid: Array[Array[Integer]]) : Unit = {
     grid.foreach(f => {
       f.foreach(i => print(i +" "))
       println()
     })
-  }
-
-  def printing_grid_alt(grid: Array[Array[Integer]]) : Unit = {
-    for {
-      i <- 0 until rows
-      j <- 0 until cols
-    } println(s"($i)($j) =${grid(i)(j)}")
   }
 
   def empty_grid() : Array[Array[Integer]] = {
@@ -48,23 +42,46 @@ object Handler {
     grid
   }
 
-  def starting_life(file: String, grid: Array[Array[Integer]]) : Array[Array[Integer]] = {
+  def starting_life(file: String) : Array[Array[Integer]] = {
     val source = scala.io.Source.fromFile(file)
     val lines = try source.getLines.toArray finally source.close()
     lines.foreach(f=> { var temp = f.split(":"); grid(temp(0).toInt)(temp(1).toInt) = 1; })
     grid
   }
 
-  def life_iteration(grid: Array[Array[Integer]]) : Array[Array[Integer]] = {
-    var new_grid = empty_grid()
+  def life_iteration() {
+    val new_grid = empty_grid()
+    for {
+      r <- 0 until rows
+      c <- 0 until cols
+    } new_grid(r)(c) = handle_neighbours(r,c)
+    grid = new_grid
+    println("Life Iteration grid:")
+    printing_grid(grid)
+  }
 
-    grid
+  def handle_neighbours(r:Integer, c:Integer): Integer = {
+    if (grid(r)(c) == 1) live_cell_out(count_neighbours(r,c))
+    else dead_cell_out(count_neighbours(r,c))
   }
 
   def count_neighbours(r:Integer, c:Integer): Integer = {
     var neighbours: Integer = 0
-
+    if (neighbour_exists(r-1,c-1)) neighbours+=grid(r-1)(c-1)
+    if (neighbour_exists(r,c-1)) neighbours+=grid(r)(c-1)
+    if (neighbour_exists(r+1,c-1)) neighbours+=grid(r+1)(c-1)
+    if (neighbour_exists(r-1,c)) neighbours+=grid(r-1)(c)
+    if (neighbour_exists(r+1,c)) neighbours+=grid(r+1)(c)
+    if (neighbour_exists(r-1,c+1)) neighbours+=grid(r-1)(c+1)
+    if (neighbour_exists(r,c+1)) neighbours+=grid(r)(c+1)
+    if (neighbour_exists(r+1,c+1)) neighbours+=grid(r+1)(c+1)
     neighbours
+  }
+
+  def neighbour_exists(r:Integer, c:Integer): Boolean = {
+    if ((0 <= r && r <= rows-1) && (0 <= c && c <= cols-1)){
+      return true
+    } else return false
   }
 
   def live_cell_out(neighbours: Integer) : Integer = {
